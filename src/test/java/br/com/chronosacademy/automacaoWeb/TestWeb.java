a559147287390c76ce7384f4e5e4a57a562e7454
Soraia testes <soraiasantos3@hotmail.com>
2024-12-16 13:56:25 -0300

    aula 08, 09 pages, estrutura e refactor

--- src/test/java/br/com/chronosacademy/automacaoWeb/TestWeb.java
+++ src/test/java/br/com/chronosacademy/automacaoWeb/TestWeb.java
@@ -1,35 +1,48 @@
 package br.com.chronosacademy.automacaoWeb;
+import br.com.chronosacademy.core.Driver;
+import br.com.chronosacademy.pages.CursoPage;
+import br.com.chronosacademy.pages.PrincipalPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertEquals;
 
 public class TestWeb {
-    ChromeDriver driver;
+    Driver driverWeb;
+    WebDriver driver;
+    PrincipalPage principalPage;
+    CursoPage cursoPage;
+
     @Before
     public void inicializaTest(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.chronosacademy.com.br");
+        principalPage = new PrincipalPage(driver);
     }
     @Test
     public void primeiroTeste(){
 
-        String xpathTitulo = "//section[2]//h4";
-    //                       "//html/body/div/div/div/section[2]/div[3]/div/div/div[1]/div/h4";
-    //                       "//section[2]/div[3]/div/div/div[1]/div//h4";
-
-        WebElement txtTitulo= driver.findElement(By.xpath(xpathTitulo));
-        String titulo = txtTitulo.getText();
+        String titulo = principalPage.getTitulo();
         assertEquals("Porque Tempo É Conhecimento.", titulo);
     }
-    @After
-    public void finalizaTest(){
-        driver.quit();
+
+    @Test
+    public void segundoTeste() {
+        cursoPage = new CursoPage(driver);
+        principalPage.clickBotao();
+        String titulo = cursoPage.getTitulo2();
+        assertEquals("Conheça todos os nossos cursos.", titulo);
     }
+
+    @After
+        public void finalizaTest () {
+            driver.quit();
+        }
 }
